3c0105f31f9293a8963062e7ffcbdf8b24f92103
Daniel Adler <weston.adler@gmail.com>
2015-11-02 17:35:56 -0800

    HTML from static site ported over unmodified

--- daniel-website-rails/config/routes.rb
+++ daniel-website-rails/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get "/"          , to: "home#index"
+  get "/about"     , to: "home#about"
+  get "/portfolio" , to: "home#portfolio"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
