8c22b79c0f6ec14ce044a153ce5726bc3d383550
Daniel Adler <weston.adler@gmail.com>
2015-11-03 11:27:47 -0800

    fixed routing error on header link back to homepage

--- daniel-website-rails/config/routes.rb
+++ daniel-website-rails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/"          , to: "home#index"
   get "/about"     , to: "home#about"
   get "/portfolio" , to: "home#portfolio"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
