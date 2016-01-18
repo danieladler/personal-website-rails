01ad8d31bded3cac348296fb8db3d9083e966abc
danieladler <weston.adler@gmail.com>
2016-01-18 13:45:58 -0800

    change font location and CSS code for display on rails-based site

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module DanielWebsiteRails
 
     # Do not swallow errors in_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
